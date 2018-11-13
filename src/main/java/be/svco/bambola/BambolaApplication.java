b976ed44fabe6ea1deaaa5038c6272a4e825ad8c
Steven Vancoilie <stevenvancoillie@hotmail.com>
2018-11-13 10:05:20 +0100

    Initial commit

---
+++ src/main/java/be/svco/bambola/BambolaApplication.java
@@ -0,0 +1,12 @@
+package be.svco.bambola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambolaApplication.class, args);
+	}
+}
