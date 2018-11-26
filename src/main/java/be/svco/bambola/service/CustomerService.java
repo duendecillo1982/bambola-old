b50e60ea5f86ca54cb20383c1ffc75692c8097e5
Steven Vancoilie <stevenvancoillie@hotmail.com>
2018-11-26 12:59:23 +0100

    update customer

--- src/main/java/be/svco/bambola/service/CustomerService.java
+++ src/main/java/be/svco/bambola/service/CustomerService.java
@@ -1,6 +1,7 @@
 package be.svco.bambola.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,12 @@ public class CustomerService {
     public List findAll() {
         return customerRepository.findAll();
     }
+    
+    public Customer findById(Long id) {
+    	Optional<Customer> optionalCustomer =  customerRepository.findById(id);
+    	Customer customer = optionalCustomer.get();
+    	return customer;
+    }
 
 	public void addCustomer(Customer customer) {
 		customerRepository.save(customer);		
@@ -24,5 +31,9 @@ public class CustomerService {
 
 	public void deleteCustomer(Long id) {
 		customerRepository.deleteById(id);		
+	}
+
+	public Customer update(Customer customer) {
+		return customerRepository.save(customer);
 	}
 }
--- src/main/java/be/svco/bambola/web/CustomerController.java
+++ src/main/java/be/svco/bambola/web/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,25 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 import be.svco.bambola.model.Customer;
 import be.svco.bambola.service.CustomerService;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping({"/customers"})
+@RequestMapping({ "/customers" })
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
+
 	@GetMapping
-	public List FindAll() {
+	public List findAll() {
 		return customerService.findAll();
 	}
+
+	@GetMapping(path = { "/{id}" })
+	public Customer findOne(@PathVariable("id") long id) {
+		return customerService.findById(id);
+	}
 	
+    @PutMapping(path = {"/{id}"})
+    public Customer update(@PathVariable("id") long id, @RequestBody Customer customer){
+    	customer.setId(id);
+        return customerService.update(customer);
+    }
+
 	@PostMapping
 	public void addCustomer(@RequestBody Customer customer) {
 		customerService.addCustomer(customer);
 	}
 	
-	@DeleteMapping(path ={"/{id}"})
+	@DeleteMapping(path = { "/{id}" })
 	public void deleteCustomer(@PathVariable Long id) {
 		customerService.deleteCustomer(id);
 	}
