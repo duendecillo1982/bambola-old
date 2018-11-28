35acba6431931e4dacdd3b5ca57bd0e22093f3d1
Steven Vancoilie <stevenvancoillie@hotmail.com>
2018-11-28 22:02:27 +0100

    model customer uitgebreid

--- src/main/java/be/svco/bambola/BambolaApplication.java
+++ src/main/java/be/svco/bambola/BambolaApplication.java
@@ -2,8 +2,10 @@ package be.svco.bambola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BambolaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/be/svco/bambola/model/Customer.java
+++ src/main/java/be/svco/bambola/model/Customer.java
@@ -1,42 +1,207 @@
 package be.svco.bambola.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name="customers")
+@Table(name = "customers")
 @EntityListeners(AuditingEntityListener.class)
 public class Customer {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String firstName;
+	
+	@Column(nullable = false)
 	private String lastName;
 	
-		public Long getId() {
+	private String companyName;
+	private String eMail;
+	private String homePhone;
+	private String mobilePhone;
+	private String fax;
+	private String address;
+	private String postalCode;
+	private String city;
+	private String countryName;
+	private String notes;
+	private Date dateCreatedOld;
+	private String familyMembers;
+	private Double amountLastPurchase;
+	private Date dateLastPurchase;
+
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date modifiedAt;
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public void setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getHomePhone() {
+		return homePhone;
+	}
+
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Date getDateCreatedOld() {
+		return dateCreatedOld;
+	}
+
+	public void setDateCreatedOld(Date dateCreatedOld) {
+		this.dateCreatedOld = dateCreatedOld;
+	}
+
+	public String getFamilyMembers() {
+		return familyMembers;
+	}
+
+	public void setFamilyMembers(String familyMembers) {
+		this.familyMembers = familyMembers;
+	}
+
+	public Double getAmountLastPurchase() {
+		return amountLastPurchase;
+	}
+
+	public void setAmountLastPurchase(Double amountLastPurchase) {
+		this.amountLastPurchase = amountLastPurchase;
+	}
+
+	public Date getDateLastPurchase() {
+		return dateLastPurchase;
+	}
+
+	public void setDateLastPurchase(Date dateLastPurchase) {
+		this.dateLastPurchase = dateLastPurchase;
+	}
+
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
 
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
